06496286734327b74bca79dec9b88bd847733741
Sebastian Gale <sebastiangale@e.email>
2019-07-22 21:49:49 +0200

    Started a "content" section that will display videos
    Started working on a section to display "video info" cards that will display the thumbnail and information of videos
    
    Also refactored the css because it was a little messy

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import './App.css';
 
+//Images
+import ThumbnailExample2 from './images/thumbnailExample2.png';
+
 //Material UI Imports
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
@@ -11,41 +14,40 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 
-const lightTheme = makeStyles({
-  logo: {
-    'padding-right': '5px',
-    'padding-left': '5px',
-    background: 'black',
-  },
-  logoText: {
+//Only colour theming goes here
+//Other css goes in ./App.css
+
+//Light theme
+const lightTheme = makeStyles({logoText: {
     color: '#00ff00',
   },
   topbar: {
     background: '#ffffff',
-    filter: 'drop-shadow(0 60 1px #222222)',
   },
   topbarText: {
     color: 'black',
   },
   hero: {
     background: '#000000',
-    border: 0,
     color: '#00c700',
-    padding: '0 30px',
   },
   heroLink: {
     color: 'white',
   },
 });
 
+//TODO dark theme
+
+//Renders the navigation bar
 function Topbar() {
   const styles = lightTheme();
 
   return (
     <div className={styles.topbar}>
-      <AppBar className={styles.topbar} position="static" color="default">
+      <AppBar className={styles.topbar} position="static">
         <Toolbar>
-          <Paper className={styles.logo}>
+          {/*Logo*/}
+          <Paper class="logo">
             <Typography className={styles.logoText} variant="h6" color="inherit">
               CyberMate
             </Typography>
@@ -56,42 +58,75 @@ function Topbar() {
   );
 }
 
+//Renders the hero section
+function Hero() {
+  const styles = lightTheme();
+
+  return (
+    /*div for the hero section of the website*/
+    <div class="hero" className={styles.hero} style={{ padding: 20 }}>
+      <Grid
+        container
+        className="heroGrid"
+        spacing={0}
+        direction="column"
+        justify="space-evenly"
+        alignItems="stretch"
+      >
+        {/*Grid items for the text in the hero section*/}
+        <Grid item xs={12}>
+          <h3 className="heroText">What is CyberMate?</h3>
+          <p className="heroText">CyberMate is an online video platform for cyber security and technological content</p>
+        </Grid>
+
+        <Grid item xs={12}>
+          <h3 className="heroText">Will the project be open source?</h3>
+          <p className="heroText">Yes, and it already is! You can find this web page on <a target="_blank" className={styles.heroLink} href="https://github.com/cybermate-tech">our GitHub!</a></p>
+        </Grid>
+
+        <Grid item xs={12}>
+          <h3 className="heroText">Can I help contribute to the project?</h3>
+          <p className="heroText">Of course! You can join us on <a target="_blank" className={styles.heroLink} href="https://discord.gg/VawZj2F">Discord</a>, and soon we'll be accepting donations through GoFundMe!</p>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+//TODO renders a card with info and thumbnail of a video
+function VideoInfo() {
+  const styles = lightTheme;
+
+  return(
+    <img class="thumbnail" src={ThumbnailExample2} />
+  );
+}
+
+//Main function to render the app as a whole
 function App() {
   const styles = lightTheme();
 
   return (
     //Root div for the website
     <div className="App">
-      {/*div for the top appbar*/}
+
       <Topbar />
 
-      {/*div for the hero section of the website*/}
-      <div className="hero" className={styles.hero} style={{ padding: 20 }}>
+      <Hero />
+
+      <div style={{ background: '#111111' }}>
         <Grid
           container
-          className="heroGrid"
-          spacing={0}
-          direction="column"
-          justify="space-evenly"
-          alignItems="stretch"
+          direction="row"
+          justify="center"
+          alignItems="center"
         >
-          {/*Grid items for the hero section*/}
-          <Grid item xs={12}>
-            <h3 className="heroText">What is CyberMate?</h3>
-            <p className="heroText">CyberMate is an online video platform for cyber security and technological content</p>
-          </Grid>
-
-          <Grid item xs={12}>
-            <h3 className="heroText">Will the project be open source?</h3>
-            <p className="heroText">Yes, and it already is! You can find this web page on <a target="_blank" className={styles.heroLink} href="https://github.com/cybermate-tech">our GitHub!</a></p>
-          </Grid>
-
-          <Grid item xs={12}>
-            <h3 className="heroText">Can I help contribute to the project?</h3>
-            <p className="heroText">Of course! You can join us on <a target="_blank" className={styles.heroLink} href="https://discord.gg/VawZj2F">Discord</a>, and soon we'll be accepting donations through GoFundMe!</p>
+          <Grid item xs={3}>
+            <VideoInfo />
           </Grid>
         </Grid>
       </div>
+
     </div>
   );
 }
